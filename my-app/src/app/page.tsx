6e00b027df45c737ea930811cb75679475baee2e
samhalsall23 <54165031+samhalsall23@users.noreply.github.com>
2024-01-05 05:32:08 +0000

    add images

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ProjectTile from "./components/ProjectTile";
 
+import ToddDesigns from "./images/WHITELOGO-jack.png"
+
 export default function Page() {
   return (
     <main>
+      <div>
+        <img src="./images/WHITELOGO-jack.png" alt='logo' />
+      </div>
       <h1>This is a</h1>
       <ProjectTile />
     </main>
