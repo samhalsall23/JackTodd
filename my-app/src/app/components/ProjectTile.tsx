369b3413bc612cb06fe1807d1f602319e28fa981
samhalsall23 <54165031+samhalsall23@users.noreply.github.com>
2024-01-09 23:34:04 +0000

    projects json

--- my-app/src/app/components/ProjectTile.tsx
+++ my-app/src/app/components/ProjectTile.tsx
@@ -6,24 +6,24 @@ interface ProjectTileProps {
   title: string;
 }
 
-export default function ProjectTile() {
+export default function ProjectTile({ image, title }: ProjectTileProps) {
   return (
     <div
       className="project-tile"
       style={{
-        backgroundImage: `url(https://images.pexels.com/photos/19644201/pexels-photo-19644201/free-photo-of-close-up-of-a-jar-of-cream.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`,
-        position: 'absolute', 
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        width: '100%', 
-        height: '100%', 
-        display: 'flex', 
-        alignItems: 'center', 
-        justifyContent: 'center',
+        backgroundImage: `url(${image})`,
+        position: "absolute",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
       }}
     >
       <div className="project-tile-content">
-        <h1>Todd's art</h1>
+        <h1>{title}</h1>
       </div>
     </div>
   );
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,35 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ProjectTile from "./components/ProjectTile";
 
+import projects from "./data/projects.json";
 
 export default function Page() {
   return (
     <main>
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom:'60px' }}>
-        <img src="https://i.ibb.co/xDGBhH7/WHITELOGO-jack.png" alt='logo' style={{ width: '50%', height: 'auto' }} />
-        <p style={{ textAlign: 'center', fontSize:'20px' }}>Multi-media graphic designer specialising in publication design, branding and advertising</p>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginBottom: "60px",
+        }}
+      >
+        <img
+          src="https://i.ibb.co/xDGBhH7/WHITELOGO-jack.png"
+          alt="logo"
+          style={{ width: "50%", height: "auto" }}
+        />
+        <p style={{ textAlign: "center", fontSize: "20px" }}>
+          Multi-media graphic designer specialising in publication design,
+          branding and advertising
+        </p>
       </div>
-      <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '1px', justifyContent: 'center', width: '90%', margin: '0 auto' }} className="outer">
-        <div style={{ backgroundColor: '#F0F0F0', paddingBottom: '100%', position: 'relative' }}>
-          <ProjectTile />
-        </div>
-        <div style={{ backgroundColor: '#F0F0F0', paddingBottom: '100%', position: 'relative' }}>
-          <ProjectTile />
-        </div>
-        <div style={{ backgroundColor: '#F0F0F0', paddingBottom: '100%', position: 'relative' }}>
-          <ProjectTile />
-        </div>
-        <div style={{ backgroundColor: '#F0F0F0', paddingBottom: '100%', position: 'relative' }}>
-          <ProjectTile />
-        </div>
-        <div style={{ backgroundColor: '#F0F0F0', paddingBottom: '100%', position: 'relative' }}>
-          <ProjectTile />
-        </div>
-        <div style={{ backgroundColor: '#F0F0F0', paddingBottom: '100%', position: 'relative' }}>
-          <ProjectTile />
-        </div>
-        
-        {/* Repeat the above div for each project tile */}
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(3, 1fr)",
+          gap: "1px",
+          justifyContent: "center",
+          width: "90%",
+          margin: "0 auto",
+        }}
+        className="outer"
+      >
+        {projects.map((project) => (
+          <div
+            style={{
+              backgroundColor: "#F0F0F0",
+              paddingBottom: "100%",
+              position: "relative",
+            }}
+            key={project.id}
+          >
+            <ProjectTile image={project.image} title={project.title} />
+          </div>
+        ))}
       </div>
     </main>
   );
