18290d967e153edef25fe1a99e0971fed1b075cc
samhalsall23 <54165031+samhalsall23@users.noreply.github.com>
2024-01-11 00:44:07 +0000

    remove not used lines

--- my-app/src/app/projects/[id]/page.tsx
+++ my-app/src/app/projects/[id]/page.tsx
@@ -1,14 +1,10 @@
-// import { useRouter } from "next/router";
+import ProjectPageContent from "../../components/ProjectPageContent";
 
-import { useParams } from "next/navigation";
-
-import ProjectPageContent from "../../components/ProjectPageContent"; // Import your Server Component
 export default function ProjectPage({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  // Pass the id as a prop to the Server Component
   return (
     <>
       <ProjectPageContent id={id} />
