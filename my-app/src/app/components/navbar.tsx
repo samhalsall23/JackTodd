2cddb3c9c41550072d48ee3c3208df40d8d46cba
samhalsall23 <54165031+samhalsall23@users.noreply.github.com>
2024-01-17 00:05:10 +0000

    initial start for sidebar

--- my-app/src/app/components/navbar.tsx
+++ my-app/src/app/components/navbar.tsx
@@ -64,7 +64,7 @@ export default function Navbar() {
         ) : (
           <div />
         )}{" "}
-        <ul>
+        <ul className="navbar-list-wrapper">
           <li>
             <Link href="/about">
               <h1>
