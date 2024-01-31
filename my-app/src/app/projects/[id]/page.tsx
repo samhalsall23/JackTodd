f864aeea0f3abb88ecb0567bd094f9bde3c61369
samhalsall23 <samhalsall23@gmail.com>
2024-01-31 20:49:50 +1100

    added files

--- my-app/src/app/projects/[id]/page.tsx
+++ my-app/src/app/projects/[id]/page.tsx
@@ -1,35 +1,56 @@
+"use client";
+import { useEffect, useState } from "react";
 import ProjectPageContent from "../../components/ProjectPageContent";
+import projects from "../../data/projects.json";
+
+interface Project {
+  id: string;
+  title: string;
+  imageSquare: string;
+  imageWide: string;
+  description: string;
+  color: string;
+}
 
 export default function ProjectPage({
   params: { id },
 }: {
   params: { id: string };
 }) {
+  const [project, setProject] = useState<Project | null>(null);
+  useEffect(() => {
+    const chosenProject: Project | undefined = projects.find(
+      (project) => project.id === id
+    );
+    console.log(chosenProject);
+    setProject(chosenProject || null);
+    console.log(project);
+  }, [project]);
+
   return (
-    <>
-      <div>
-        <div className="hero-image">
-          <img
-            src="/assets/projects/2-imagemaker/IMAGEMAKER-WIDE.png"
-            alt="Hero Image"
-            style={{
-              width: "100%",
-              height: "auto",
-            }}
-          />
-        </div>
-        <div
+    <div>
+      <div className="hero-image">
+        {project && <>{console.log(project.imageWide)}</>}
+        <img
+          src={project?.imageWide}
+          alt="Hero Image"
           style={{
-            position: "fixed",
-            marginTop: "20px",
-            display: "flex",
-            justifyContent: "center",
             width: "100%",
+            height: "auto",
           }}
-        >
-          <ProjectPageContent id={id} />
-        </div>
+        />
       </div>
-    </>
+      <div
+        style={{
+          position: "fixed",
+          marginTop: "20px",
+          display: "flex",
+          justifyContent: "center",
+          width: "100%",
+        }}
+      >
+        <ProjectPageContent id={id} />
+      </div>
+    </div>
   );
 }
