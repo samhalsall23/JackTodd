d0c005195824368d0d54ffd0d8a0229858f40d6e
samhalsall23 <54165031+samhalsall23@users.noreply.github.com>
2024-02-02 02:58:49 +0000

    contact first attempt

--- my-app/src/app/contact/page.tsx
+++ my-app/src/app/contact/page.tsx
@@ -1,9 +1,35 @@
 import React from "react";
 
+import "../styles/Contact.scss";
+
 export default function Contact() {
   return (
     <main>
-      <h1>Contact me</h1>
+      <div className="form-container">
+        <img
+          className="hero-logo"
+          src="assets\logo\WHITELOGO-jack.png"
+          alt="logo"
+        />
+
+        <form>
+          <div className="form-group">
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">Message:</label>
+            <textarea rows={4} id="message" name="message" />
+          </div>
+          <button type="submit" className="submit-button">
+            Send
+          </button>
+        </form>
+      </div>
     </main>
   );
 }
