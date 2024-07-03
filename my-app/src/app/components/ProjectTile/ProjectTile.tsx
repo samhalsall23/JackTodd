b932edc6866740528c7ad6b928d81405a16b726f
samhalsall23 <samhalsall23@gmail.com>
2024-07-03 20:15:52 +1000

    all next images

--- my-app/src/app/components/ProjectTile/ProjectTile.tsx
+++ my-app/src/app/components/ProjectTile/ProjectTile.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useRef, useState } from "react";
-
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import "./project-tile-styles.scss";
 
@@ -54,14 +54,37 @@ export default function ProjectTile({
 
   return (
     <div style={{ display: "flex", justifyContent: "center" }}>
+      <Image
+        style={{
+          position: "absolute",
+          top: "0",
+          left: "0",
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          zIndex: "0",
+        }}
+        src={imageSquare}
+        alt="Hero Image"
+        height={4167}
+        width={4167}
+        quality={75} // Adjusted for better performance
+        // priority // Use this for above-the-fold images only
+        sizes="(max-width: 480): 100vw,
+                (max-width: 992px): 50vw,
+                33vw"
+        // sizes="100vw"
+      />
       <div
-        ref={tileRef}
+        // ref={tileRef}
         className={
           "project-tile" + (isCarousel ? " project-tile-carousel" : "")
         }
-        style={{
-          backgroundImage: `url(${imageSquare})`,
-        }}
+        style={
+          {
+            // backgroundImage: `url(${imageSquare})`,
+          }
+        }
         onClick={onProjectTileClick}
       >
         <div
