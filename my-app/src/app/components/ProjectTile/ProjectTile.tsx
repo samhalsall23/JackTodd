4338b60c5e49ae28c86ca26e24f376a6bbda8b6d
samhalsall23 <samhalsall23@gmail.com>
2024-07-04 23:05:39 +1000

    fix carousel bug

--- my-app/src/app/components/ProjectTile/ProjectTile.tsx
+++ my-app/src/app/components/ProjectTile/ProjectTile.tsx
@@ -54,7 +54,7 @@ export default function ProjectTile({
 
   return (
     <div style={{ display: "flex", justifyContent: "center" }}>
-      <Image
+      {/* <Image
         style={{
           position: "absolute",
           top: "0",
@@ -74,26 +74,57 @@ export default function ProjectTile({
                 (max-width: 992px): 50vw,
                 33vw"
         // sizes="100vw"
-      />
+      /> */}
       <div
         // ref={tileRef}
-        className={
-          "project-tile" + (isCarousel ? " project-tile-carousel" : "")
-        }
-        style={
-          {
-            // backgroundImage: `url(${imageSquare})`,
-          }
-        }
+        className={"project-tile"}
+        style={{
+          zIndex: "1",
+          // backgroundImage: `url(${imageSquare})`,
+        }}
         onClick={onProjectTileClick}
       >
         <div
-          className={
-            "project-tile-content " +
-            (isCarousel ? "project-tile-content-carousel" : "")
+          // className={isCarousel ? "project-tile-content-carousel" : ""}
+          style={
+            isCarousel
+              ? { width: "90%", height: "90%", position: "relative" }
+              : { width: "100%", height: "100%", position: "relative" }
           }
         >
-          <h1>{title}</h1>
+          <Image
+            className={isCarousel ? "project-tile-carousel" : ""}
+            style={{
+              position: "absolute",
+              top: "0",
+              left: "0",
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+              zIndex: "-1",
+            }}
+            src={imageSquare}
+            alt="Hero Image"
+            height={4167}
+            width={4167}
+            quality={75} // Adjusted for better performance
+            // priority // Use this for above-the-fold images only
+            sizes={
+              // "100vw"
+              isCarousel
+                ? "(max-width: 768px) 100vw, (max-width: 1024px) 50vw, (max-width: 1920px) 33vw, 25vw"
+                : "(max-width: 480px) 100vw, (max-width: 992px) 50vw, 33vw"
+            }
+            // sizes="100vw"
+          />
+          <div
+            className={
+              "project-tile-content " +
+              (isCarousel ? "project-tile-content-carousel" : "")
+            }
+          >
+            <h1>{title}</h1>
+          </div>
         </div>
       </div>
     </div>
